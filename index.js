655b8830e767c368536f476424722cc6f66e363e
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-24 06:42:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {title = title.split(" ");
+const titleCased = () => {
+  let title = title.split(" ");
     for (var i = 0, x = title.length; i < x; i++) {
         title[i] = title[i][0].toUpperCase() + title[i].substr(1);
     }
